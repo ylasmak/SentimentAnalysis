59525059a41601036ba4d06220d646ec5c44689f
ylasmak <machallah@412>
2016-08-25 17:17:03 +0100

    commit

--- Script/TweeterScraping/TweeterScraping.R
+++ Script/TweeterScraping/TweeterScraping.R
@@ -14,20 +14,20 @@ api_secret = 'eKNDz9u0BG8OCIDX8pXoyrdtbqXMiAq2lx68FkamOS8rjpYQSn'
 access_token = '49266784-6ADI5apTARq56TJOpdBirDZk4gzmoByktYauxdCd8'
 access_token_secret = 'HT4c5ZualavTzEF12QbT6oelws3c3dZ748h6wQfBFII6J'
 
-setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
-
-some_tweets = searchTwitter('starbucks', n=10000, lang='en')
 
 
-
-write.csv(twListToDF(data), file="something.csv")
+some_tweets = searchTwitter('nespresso', n=10000, lang='en')
 
 
 
-# get the text
-some_txt = sapply(some_tweets, function(x) x$getText())
+df <- twListToDF(some_tweets)
 
 
 
+#require(devtools)
+#install_url("https://cran.r-project.org/src/contrib/Archive/Rstem//Rstem_0.4-1.tar.gz")
+#install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.1.tar.gz")
+#install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.2.tar.gz")
 
-save(some_tweets ,file = './Data/tweet.txt')
+#writeLines(df$text, file="./Data/starbucks.txt")
+#write(df$text,file="./Data/iphone.txt" )
